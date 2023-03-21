aaab48631410321a68d295cfa64ed708c25a45c1
Nikolay Berdnikov <n.a.berdnikov@gmail.com>
2023-03-21 20:46:45 +0300

    small cleanup

--- app/js/script.js
+++ app/js/script.js
@@ -46,8 +46,6 @@ filterOption.forEach(function (el) {
   el.addEventListener('click', chooseOption)
 })
 
-// filterOption.map(el => el.addEventListener('click', chooseOption))
-
 function convertDate(date) {
   const d = new Date(date)
   return `${String(d.getDate()).padStart(2, '0')}.${String(
